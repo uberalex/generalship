69458925ca038569172f0749324144ff0f49bcad
Alex O'Connor <oconnoat@gmail.com>
2012-07-27 18:01:57 +0100

    revised

--- js/app.js
+++ js/app.js
@@ -1,19 +1,29 @@
-var App = Em.Application.create();
+var map = d3.select("#map")
+            .append("svg")
+            .attr("width",500)
+            .attr("height",500);
 
-App.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
-  },
-  didInsertElement: function(){
-    var rectDemo = d3.select("#chart").
-  append("svg:svg").
-  attr("width", 400).
-  attr("height", 300);
+var coords = [ [1,2], [1,3], [2,4], [2,5], [3,6], [3,7], [4,8], [4,9] ];
 
-rectDemo.append("svg:rect").
-  attr("x", 100).
-  attr("y", 100).
-  attr("height", 100).
-  attr("width", 200);
-  }
-});
+var nodes = {[
+                {"x":"1",
+                 "y":"1",
+                 "r":"5",
+                 "name":"one"},
+                {"x":"2",
+                 "y":"2",
+                 "r":"10",
+                 "name":"two"},
+                {"x":"3",
+                 "y":"3",
+                 "r":"20",
+                 "name":"three"}
+            ]};
+
+var nodes = map.selectAll("circle")
+            .json(nodes)
+            .enter()
+            .append("circle")
+            .attr("cx",function(d) { return d.x * 50; })
+            .attr("cy",function(d) { return d.y * 50; })
+            .attr("r", function(d) { return d.r; });
